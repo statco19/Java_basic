429e69df2732ff96ca68ead45a72064e30ca3476
Stephen <statco19@gmail.com>
2020-10-18 21:29:24 +0900

    Add files via upload
    10/18

---
+++ ch4/FlowEx17.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class FlowEx17{
+
+    public static void main(String args[]){
+		int num = 0;
+		
+		System.out.println("*을 출력할 라인의 수를 입력하세요.>");
+		
+       Scanner scn = new Scanner(System.in);
+	   String tmp = scn.nextLine();
+	   num = Integer.parseInt(tmp);
+	   
+		for(int i=1;i<=num;i++){
+			for(int j=1;j<=i;j++){
+				System.out.printf("*");
+			}
+			System.out.println();
+		}
+    } // main
+}
+
+
+
