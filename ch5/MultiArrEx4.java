27379992cf4a205b68971d71ca5dca6f60ee0a8d
Stephen <statco19@gmail.com>
2020-11-24 16:01:56 +0900

    Add files via upload
    11/24

---
+++ ch5/MultiArrEx4.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class MultiArrEx4{
+
+    public static void main(String args[]){
+        String[][] words = {
+			{"chair", "의자"},
+			{"computer", "컴퓨터"},
+			{"integer", "정수"}
+		};
+		
+		Scanner scn = new Scanner(System.in);
+		
+		for(int i=0;i<words.length;i++){
+			System.out.printf("Q%d, %s의 뜻은?%n", i+1, words[i][0]);
+			
+			String tmp = scn.nextLine();
+			if(tmp.equals(words[i][1])){
+				System.out.printf("정답입니다.%n%n");
+			}else{
+				System.out.printf("틀렸습니다. 정답은 %s입니다.%n%n", words[i][1]);
+			}
+		}// for i
+		
+		int lthOfWords = words.length;
+		int lthOfWordsi = words[0].length;
+		int lthOfWordsiofi = words[0][0].length();
+		
+		System.out.printf("%d %d %d%n", lthOfWords, lthOfWordsi, lthOfWordsiofi);
+    }
+}
+
+
+
