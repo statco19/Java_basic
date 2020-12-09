ca97cf60d40f042b219232fe8d96ece7b7169964
Stephen <statco19@gmail.com>
2020-12-09 19:50:42 +0900

    Add files via upload
    12/9

---
+++ ch6/ConstructorTest.java
@@ -0,0 +1,21 @@
+class Data1 { int value; }
+
+class Data2 {
+	int Value;
+	
+	Data2(int x) { // 매개변수가 있는 생성자.
+	Value = x;
+	}
+}
+
+public class ConstructorTest{
+
+    public static void main(String args[]){
+        Data1 d1 = new Data1();
+		Data2 d2 = new Data2(10);
+		System.out.println(d2.Value);
+    }
+}
+
+
+
