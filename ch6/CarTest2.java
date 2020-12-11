5a8aff89032a4d991aee6e5df4d8ca4d9259b5b4
Stephen <statco19@gmail.com>
2020-12-12 08:05:10 +0900

    Add files via upload
    12/11

---
+++ ch6/CarTest2.java
@@ -0,0 +1,34 @@
+class Car {
+	String color;
+	String gearType;
+	int door;
+	
+	Car () {
+		this("white", "auto", 4);
+	}
+	
+	Car (String color) {
+		this(color, "auto", 4);
+	}
+	
+	Car(String c, String g, int d) {
+		this.color = c;
+		this.gearType = g;
+		this.door = d;
+	}
+}
+
+public class CarTest2{
+
+    public static void main(String args[]){
+
+		Car c1 = new Car();
+		Car c2 = new Car("blue");
+		
+		System.out.println("c1의 color=" + c1.color + ", gearType=" + c1.gearType + ", door=" + c1.door);
+		System.out.println("c2의 color=" + c2.color + ", gearType=" + c2.gearType + ", door=" + c2.door);        
+    }
+}
+
+
+
